74ae22369facc6a1d01a29cfff380ff3c9524ac7
georgepro888 <georgepro8@hotmail.com>
2025-06-11 05:36:03 +0200

    rotate x and y

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import { motion } from "framer-motion"
 
 const App = () => {
   return <div>
-    <motion.div className="box bg-amber-600"  id="box1" animate={{ x: 100 }} />
-    <motion.div className="box bg-blue-600" id="box1" animate={{ y: 100 }} />
-    <motion.div className="box bg-red-600" id="box1" animate={{ translate: "200px" }} />
+    <motion.div className="box bg-amber-600"  animate={{ x: "100rem" }} />
+    <motion.div className="box bg-green-600"  animate={{ x: "10%" }} />
+    <motion.div className="box bg-blue-600" animate={{ y: 10 }} />
+    <motion.div className="box bg-red-600" animate={{ translate: "200px" }} />
+    
+    
+    <motion.div className="box1 bg-amber-600" />
+    <motion.div className="box1 bg-yellow-600" animate={{ rotateY: 50 }} />
+    <motion.div className="box1 bg-red-600" animate={{ rotateX: 50 }} />
+    <motion.div className="box1 bg-purple-600" animate={{ rotate: 50 }} />
+
+
   </div>;
 
 };
