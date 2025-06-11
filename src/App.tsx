c53b0664b78db42f7f8aa5215ea10046e10c5f8e
georgepro888 <georgepro8@hotmail.com>
2025-06-11 05:24:47 +0200

    translate x and y

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { motion } from "framer-motion"
 
 const App = () => {
   return <div>
-    <motion.div className="box" />
+    <motion.div className="box bg-amber-600"  id="box1" animate={{ x: 100 }} />
+    <motion.div className="box bg-blue-600" id="box1" animate={{ y: 100 }} />
+    <motion.div className="box bg-red-600" id="box1" animate={{ translate: "200px" }} />
   </div>;
 
 };
